f109080bfd9a5e0f6d46d92dbb9a64a5bb4cb2ba
Andrii Denysenko <andrey2850@gmail.com>
2020-07-18 19:36:04 -0400

    Refactor and separate concerns

--- Assets/Scripts/EnemyController.cs
+++ Assets/Scripts/EnemyController.cs
@@ -5,40 +5,17 @@ using Zenject;
 
 public class EnemyController : MonoBehaviour
 {
-    private float _timeBeforeShot;
-
     [Range(1, 10)]
     public float velocity;
-    public float pauseBetweenShots;
+    public float gunHeight;
+    public float gunPower;
 
     [Inject] Player player;
-    [Inject] DiContainer Container;
-
-
-    private void Start()
-    {
-        _timeBeforeShot = pauseBetweenShots;
-    }
 
     private void Update()
     {
         float deltaDistance = velocity * Time.deltaTime * GameManager.TimeScale;
         transform.position = Vector3.MoveTowards(transform.position, player.transform.position, deltaDistance);
-
-        _timeBeforeShot -= Time.deltaTime * GameManager.TimeScale;
-        if (_timeBeforeShot < 0)
-        {
-            _timeBeforeShot = pauseBetweenShots;
-            Shoot();
-            Debug.Log("EXTERMINATE!!!");
-        }
-    }
-
-    private void Shoot()
-    {
-        Vector3 bulletSpawn = transform.position + 1.5f * Vector3.up;
-        BulletController bulletController = Container.InstantiatePrefabResourceForComponent<BulletController>("Bullet", bulletSpawn, Quaternion.identity, transform);
-        bulletController.direction = player.transform.position - transform.position;
-        bulletController.velocity = 20;
+        transform.LookAt(player.transform);
     }
 }
---
+++ Assets/Scripts/Weapon.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Zenject;
+
+public class Weapon : MonoBehaviour
+{
+    private float _timeBeforeShot;
+
+    public GameObject missileResource;
+    public float pauseBetweenShots;
+    public float power;
+
+    [Inject] Player player;
+    [Inject] DiContainer Container;
+
+    void Start()
+    {
+        _timeBeforeShot = pauseBetweenShots;
+    }
+
+    void Update()
+    {
+        _timeBeforeShot -= Time.deltaTime * GameManager.TimeScale;
+        if (_timeBeforeShot < 0)
+        {
+            _timeBeforeShot = pauseBetweenShots;
+            Shoot();
+            Debug.Log("EXTERMINATE!!!");
+        }
+    }
+
+    private void Shoot()
+    {
+        Vector3 bulletSpawn = transform.position;
+        BulletController bulletController = Container.InstantiatePrefabForComponent<BulletController>(missileResource, bulletSpawn, Quaternion.identity, transform);
+        bulletController.direction = player.transform.position - transform.position;
+        bulletController.velocity = power;
+    }
+}
