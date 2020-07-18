49d1fd806818677849ed96e8a34ec07d095dc751
Gleb89 <GlebLobach@gmail.com>
2020-07-18 16:47:02 +0300

    Zenject

--- Assets/Scripts/CompassInit.cs
+++ Assets/Scripts/CompassInit.cs
@@ -1,18 +1,27 @@
 ﻿using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class CompassInit : MonoBehaviour
 {
+    public static bool compassInitialized = false;
     // Start is called before the first frame update
     void Start()
     {
-        
+        InitCompass();
+         
+    }
+    public void InitCompass()
+    {
+        Input.location.Start();
+        StartCoroutine(WaitFOrUsersAccept());
+    }
+ 
+    IEnumerator WaitFOrUsersAccept()
+    {
+        yield return new WaitUntil(() => Input.location.isEnabledByUser);
+        Input.compass.enabled = true;
+        compassInitialized = true;
+        Debug.Log("Initialized location, compass");
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
 }
