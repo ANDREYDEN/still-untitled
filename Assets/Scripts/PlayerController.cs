69b508654c88448063c8daa94e1003f3efea16cd
Andrii Denysenko <andrey2850@gmail.com>
2020-07-18 08:12:09 -0400

    Add player controller

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    const float DELTA_Y = 0.1f;
+    private float _cameraY = 0;
+
+    void Update()
+    {
+        _cameraY = GetCameraRotation();
+
+        Camera.main.transform.eulerAngles = Vector3.up * _cameraY;
+    }
+
+    private float GetCameraRotation()
+    {
+        if (Application.platform == RuntimePlatform.WindowsEditor)
+        {
+            if (Input.GetKey(KeyCode.LeftArrow))
+            {
+                return _cameraY + DELTA_Y;
+            }
+            if (Input.GetKey(KeyCode.RightArrow))
+            {
+                return _cameraY - DELTA_Y;
+            }
+        }
+        if (Application.platform == RuntimePlatform.Android)
+        {
+            return Input.compass.magneticHeading;
+        }
+        return _cameraY;
+    }
+}
