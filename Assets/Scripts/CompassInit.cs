083ce3b9f7c94a5f46327ae385125fce3eb49c54
Gleb89 <GlebLobach@gmail.com>
2020-07-19 11:44:07 +0300

    Revert "Zenject signals set up"
    This reverts commit 37ee3bf292a40cd056d5588da95b4bf5c0eea2dd.

--- Assets/Scripts/CompassInit.cs
+++ Assets/Scripts/CompassInit.cs
@@ -1,29 +1,12 @@
 ﻿using System.Collections;
 using UnityEngine;
-using Zenject;
+
 public class CompassInit : MonoBehaviour
 {
     public static bool compassInitialized = false;
-
-
-    [Inject] SignalBus _signalBus;
-    [Inject]
-    private void OnInject()
-    {
-
-        _signalBus.Subscribe<TestSignal>(OnTest);
-    }
-
-    private void OnTest(TestSignal test)
-    {
-        Debug.Log("Texted " + test.testText);
-        
-    }
-
     // Start is called before the first frame update
     void Start()
     {
-        _signalBus.Fire(new TestSignal("this is texted"));
         InitCompass();
          
     }
@@ -38,7 +21,6 @@ public class CompassInit : MonoBehaviour
         yield return new WaitUntil(() => Input.location.isEnabledByUser);
         Input.compass.enabled = true;
         compassInitialized = true;
-        _signalBus.Fire(new CompassInitializedSignal());
         Debug.Log("Initialized location, compass");
     }
 
