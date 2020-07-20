506edbb5749c338079124f75567199618bcc571d
Andrii Denysenko <andrey2850@gmail.com>
2020-07-20 00:03:05 -0400

    Add range parameter to enemies

--- Assets/Scripts/EnemyController.cs
+++ Assets/Scripts/EnemyController.cs
@@ -7,13 +7,22 @@ public class EnemyController : MonoBehaviour
 {
     [Range(0, 5)]
     public float velocity;
+    public float visibleRadius;
 
     [Inject] Player player;
 
+    private bool CanSee(Vector3 position)
+    {
+        return Vector3.Distance(transform.position, position) < visibleRadius;
+    }
+
     private void Update()
     {
-        float deltaDistance = velocity * Time.deltaTime * GameManager.TimeScale;
-        transform.position = Vector3.MoveTowards(transform.position, player.transform.position, deltaDistance);
-        transform.LookAt(player.transform);
+        if (CanSee(player.transform.position))
+        {
+            float deltaDistance = velocity * Time.deltaTime * GameManager.TimeScale;
+            transform.position = Vector3.MoveTowards(transform.position, player.transform.position, deltaDistance);
+            transform.LookAt(player.transform);
+        }
     }
 }
