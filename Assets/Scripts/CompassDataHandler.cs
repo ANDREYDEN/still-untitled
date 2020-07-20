9b50dfc3fbfa0a13f1fe2dd99dd1794906762120
Andrii Denysenko <andrey2850@gmail.com>
2020-07-20 00:42:15 -0400

    Tweaked camera controls

--- Assets/Scripts/CompassDataHandler.cs
+++ Assets/Scripts/CompassDataHandler.cs
@@ -9,28 +9,9 @@ public class CompassDataHandler : MonoBehaviour
     public GameObject player;
 
     private float dest;
-    [Inject] SignalBus _signalBus;
-
-    [Inject] 
-    private void OnInject()
-    {
-        _signalBus.Subscribe<CompassInitiated>(OnCompassInitialized);
-    }
 
     void Start()
     {
-        dest = Input.compass.trueHeading;
-        StartCoroutine(checkMovement());
-    }
-    
-    private void OnCompassInitialized()
-    {
-        //StartCoroutine(checkMovement());
-    }
-
-    IEnumerator checkMovement()
-    {
-        yield return new WaitUntil(() => Mathf.Abs(Input.compass.trueHeading - player.transform.eulerAngles.y) > 10);
         dest = Input.compass.trueHeading;
     }
 
@@ -38,10 +19,17 @@ public class CompassDataHandler : MonoBehaviour
     {
         if (Application.platform == RuntimePlatform.Android)
         {
-            //if (Time.time > 20)
-            //{
+            if (Time.time > 20)
+            {
                 dest = (int)(Input.compass.trueHeading / 10) * 10;
-            //}
+            }
+            else
+            {
+                if (Mathf.Abs(Input.compass.trueHeading - player.transform.eulerAngles.y) > 10)
+                {
+                    dest = Input.compass.trueHeading;
+                }
+            }
             player.transform.eulerAngles = Vector3.Lerp(player.transform.eulerAngles, Vector3.up * dest, Time.deltaTime);
         }
     }
